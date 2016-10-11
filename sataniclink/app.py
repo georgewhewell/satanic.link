beeaeeec4b3a993929a9d5c0205c38efd20fe0ca
George Whewell <grw@FUCKUP.local>
2016-10-11 20:21:32 +0100

    refactor a bit

--- sataniclink/app.py
+++ sataniclink/app.py
@@ -1,27 +1,40 @@
-from os.path import join, dirname
 from aiohttp import web
+
+from os.path import join, dirname
+
 
 url_map = {}
 
 index_filename = join(dirname(__file__), 'index.html')
 homepage = open(index_filename, 'rb').read()
 
-index = lambda x: web.Response(body=homepage, content_type='text/html')
+
+async def index(request):
+    return web.Response(body=homepage, content_type='text/html')
 
 
 async def submit(request):
     url_map[request.post.get('url')] = 'something'
-    return 
+    return 'something'
 
-async def handle(request):
+
+async def redirect(request):
     name = request.match_info.get('code')
     text = "ur a fuckface " + name
     return web.Response(body=homepage, content_type='text/html')
 
 
 app = web.Application()
-app.router.add_get('/', index)
-app.router.add_post('/', submit)
-app.router.add_get('/{code}', handle)
 
-web.run_app(app)
+# get index
+app.router.add_get('/', index)
+
+# shortened url
+app.router.add_get('/{code}', redirect)
+
+# submit new url
+app.router.add_post('/', submit)
+
+
+if __name__ == '__main__':
+    web.run_app(app)
